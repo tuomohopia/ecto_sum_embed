d61964a4ff00050f61ebc3a314f0434c25c8dbc7
Tuomo Hopia <tuomo.hopia@gmail.com>
2020-10-11 01:14:03 +0300

    POC part 1: ecrete embedded module dynamically

--- lib/ecto_sum_embeds.ex
+++ lib/ecto_sum_embeds.ex
@@ -1,18 +1,77 @@
 defmodule EctoSumEmbeds do
   @moduledoc """
-  Documentation for `EctoSumEmbeds`.
+  Adds `:embeds_one_of` functionality to Ecto Schema
+
+  Also brings `cast_sum_embed/3` to scope.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EctoSumEmbeds.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @doc false
+  defmacro __using__(_) do
+    quote do
+      import EctoSumEmbeds
+    end
   end
+
+  defmacro embeds_one_of(name, [schema, _]) do
+    IO.inspect(schema, label: "schema before")
+    schema = expand_alias(schema, __CALLER__)
+    IO.inspect(schema, label: "schema after")
+
+    caller = __CALLER__.module
+    IO.inspect(caller, label: "caller module")
+
+    module_name =
+      name
+      |> Atom.to_string()
+      |> Macro.camelize()
+      |> String.to_atom()
+
+    IO.inspect(module_name, label: "module name")
+
+    quote do
+      {:module, module, _binary, _term} =
+        EctoSumEmbeds.create_embedded_module(unquote(name), unquote(caller), unquote(schema))
+
+      IO.inspect(module, label: "created module")
+
+      Ecto.Schema.__embeds_one__(__MODULE__, unquote(name), module, [])
+    end
+  end
+
+  def create_embedded_module(field, base_module, _attrs) do
+    module_name =
+      field
+      |> Atom.to_string()
+      |> Macro.camelize()
+
+    module = Atom.to_string(base_module) <> "." <> module_name
+    module = String.to_atom(module)
+
+    ast =
+      quote do
+        use Ecto.Schema
+        alias Ecto.Changeset
+
+        # Add sum type members to generated module attributes
+        Module.register_attribute(__MODULE__, :ecto_sum_embeds, accumulate: true)
+
+        @primary_key false
+        embedded_schema do
+        end
+
+        def changeset(module, params) do
+          IO.inspect(module, label: "changeset module")
+          IO.inspect(module, label: "changeset params")
+          Changeset.cast(module, params, [])
+        end
+      end
+
+    Module.create(module, ast, Macro.Env.location(__ENV__))
+  end
+
+  defp expand_alias({:__aliases__, _, _} = ast, env),
+    do: Macro.expand(ast, %{env | function: {:__schema__, 2}})
+
+  defp expand_alias(ast, _env),
+    do: ast
 end
--- test/ecto_sum_embeds_test.exs
+++ test/ecto_sum_embeds_test.exs
@@ -2,22 +2,68 @@ defmodule EctoSumEmbedsTest do
   use ExUnit.Case
 
   describe "Works" do
-    defmodule TestSchema do
+    defmodule Boolean do
       use Ecto.Schema
       import Ecto.Changeset
 
-      schema "test_schema" do
-        field(:name, :string)
+      @primary_key false
+      embedded_schema do
+        field :answer, :boolean
+      end
+
+      def changeset(attrs) do
+        %__MODULE__{}
+        |> cast(attrs, [:answer])
+      end
+    end
+
+    defmodule ChooseOne do
+      use Ecto.Schema
+      import Ecto.Changeset
+
+      @primary_key false
+      embedded_schema do
+        field :one, :string
+      end
+
+      def changeset(schema \\ %__MODULE__{}, attrs) do
+        schema
+        |> cast(attrs, [:one])
+      end
+    end
+
+    defmodule Answer do
+      use EctoSumEmbeds
+      use Ecto.Schema
+      import Ecto.Changeset
+
+      schema "answers" do
+        field :name, :string
+        embeds_one_of :answer, [ChooseOne, Boolean]
+        embeds_one :choose_one_normal, ChooseOne
       end
 
       def changeset(attrs) do
         %__MODULE__{}
         |> cast(attrs, [:name])
+        |> cast_embed(:choose_one_normal)
+        |> cast_embed(:answer)
       end
     end
 
     test "Changeset works" do
-      assert %Ecto.Changeset{} = TestSchema.changeset(%{name: "Elvis"})
+      attrs = %{
+        name: "Elvis",
+        answer: %{tag: "choose_one", one: "happy"},
+        choose_one_normal: %{one: "Good"}
+      }
+
+      assert %Ecto.Changeset{} = Answer.changeset(attrs)
+      # Answer.Answer.hello()
+
+      IO.inspect(Answer.changeset(attrs) |> Ecto.Changeset.apply_changes(),
+        label: "Changeset after applying"
+      )
     end
   end
 end
